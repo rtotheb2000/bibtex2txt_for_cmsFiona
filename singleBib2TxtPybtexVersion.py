81de4481309b6bd305ef23a20c6281fd3ce3b9c4
mq <mquade@uni-potsdam.de>
2016-05-23 14:15:32 +0200

    .

--- singleBib2TxtPybtexVersion.py
+++ singleBib2TxtPybtexVersion.py
@@ -10,7 +10,6 @@ KEYS = "ID", "title", "author", "year", "month", "partOf", "source"
 
 
 class Bib2Str:
-
     def __init__(self, bib_string):
         self.bib_str = bib_string
         self.bib_str_split = ""
@@ -19,7 +18,6 @@ class Bib2Str:
         self.strip_at_and_end()
         self.get_type()
         self.split_bib_str()
-
     def strip_at_and_end(self):
         if self.bib_str == "":
             print("self.bib_str is empty. check your bib entry.")
@@ -30,8 +28,7 @@ class Bib2Str:
             elif self.bib_str[-3:] == "\"\n}":
                 self.bib_str = self.bib_str[:-4]
             else:
-                print(
-                    "end of bib string could not be subtracted due to unrecognized format. conversion will not work.")
+                print("end of bib string could not be subtracted due to unrecognized format. conversion will not work.")
 
     def get_type(self):
         self.bib_type = (re.split("{", self.bib_str)[0]).lower()
@@ -48,17 +45,15 @@ class Bib2Str:
         self.dictionary = dictionary
 
     def write_txt(self):
-        if self.dictionary == "":
-            print(
-                "Nothing to write. Load a bib file using read_bib() before you use this module.")
+        if not self.dictionary:
+            print("Nothing to write. Load a bib file using read_bib() before you use this module.")
         else:
             try:
                 os.remove((self.dictionary["ID"]).lower() + ".txt")
-            except:
+            except IOError:
                 pass
-            if not all(key in KEYS for key in self.dictionary.keys()):
-                raise AttributeError(
-                    "There is at least one required argument missing {}".format(KEYS))
+            if len(self.dictionary.keys()) < 7:
+                raise AttributeError("There is at least one required argument missing {}. Available keys: {}".format(KEYS, self.dictionary.keys()))
             else:
                 with io.open((self.dictionary["ID"]).lower() + ".txt", "w", encoding='utf8') as bib2txt_file:
                     bib2txt_file.write(
@@ -75,37 +70,30 @@ class Bib2Str:
                             monthnr = ["jan", "feb", "mar", "apr", "may", "jun", "jul",
                                        "aug", "sep", "oct", "nov", "dec"].index(self.dictionary[i]) + 1
                             if monthnr < 10:
-                                bib2txt_file.write(
-                                    str(0) + str(monthnr) + "\t")
+                                bib2txt_file.write(str(0) + str(monthnr) + "\t")
                             else:
                                 bib2txt_file.write(str(monthnr) + "\t")
                         elif i == "partOf":
                             try:
-                                bib2txt_file.write(self.dictionary[
-                                                   "journal"] + " " + self.dictionary["volume"] + ", " + self.dictionary["pages"] + "\t")
+                                bib2txt_file.write(self.dictionary["journal"] + " " + self.dictionary["volume"] + ", " + self.dictionary["pages"] + "\t")
                             except:
                                 try:
-                                    bib2txt_file.write(
-                                        self.dictionary["booktitle"] + ", " + self.dictionary["pages"] + "\t")
+                                    bib2txt_file.write(self.dictionary["booktitle"] + ", " + self.dictionary["pages"] + "\t")
                                 except:
-                                    print(
-                                        "self.dictionary[", i, "] ", "returned error")
+                                    print("self.dictionary[", i, "] ", "returned error")
                         elif i == "source":
                             try:
-                                bib2txt_file.write(
-                                    "http://dx.doi.org/" + self.dictionary["doi"])
+                                bib2txt_file.write("http://dx.doi.org/" + self.dictionary["doi"])
                             except:
                                 try:
                                     bib2txt_file.write(self.dictionary["url"])
                                 except:
-                                    print(
-                                        "self.dictionary[", i, "] ", "returned error")
+                                    print("self.dictionary[", i, "] ", "returned error")
                         else:
                             try:
                                 bib2txt_file.write(self.dictionary[i] + "\t")
                             except:
-                                print(
-                                    "self.dictionary[", i, "] ", "returned error")
+                                print("self.dictionary[", i, "] ", "returned error")
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
